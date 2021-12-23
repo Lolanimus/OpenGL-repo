a301b54d60634afef6082ad7a895821f6bc31346
Lolanimus <antox.qscwdv@gmail.com>
2021-12-24 00:23:55 +0800

    Updated mesh.h and created model.h for importing models

---
+++ OpenGLApplication/model.h
@@ -0,0 +1,60 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <glad/glad.h> 
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "stb_image.h"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include "mesh.h"
+#include "shader.h"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+class Model
+{
+	public:
+		Model(char *path)
+		{
+			loadModel(path);
+		}
+		void Draw(Shader& shader)
+		{
+			for (unsigned int i = 0; i < meshes.size(); i++)
+				meshes[i].Draw(shader);
+		}
+	
+	private:
+		// Model data
+		vector<Mesh> meshes;
+		string directory;
+
+		void loadModel(string path)
+		{
+			Assimp::Importer importer;
+			const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+			if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+			{
+				cout << "ERROR::ASSIMP::" << importer.GetErrorString() << endl;
+				return;
+			}
+			directory = path.substr(0, path.find_last_of('/'));
+
+			processNode(scene->mRootNode, scene);
+		}
+		void processNode(aiNode *node, const aiScene *scene);
+		Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+		vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, string typeName);
+};
+#endif
