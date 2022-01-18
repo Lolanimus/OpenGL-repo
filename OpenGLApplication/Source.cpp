1c544b6dead61f5846a05dd2d1f18360f8d2f3fc
Lolanimus <antox.qscwdv@gmail.com>
2022-01-18 19:39:08 +0800

    Added fbo MSAA

--- OpenGLApplication/Source.cpp
+++ OpenGLApplication/Source.cpp
@@ -18,15 +18,17 @@ unsigned int loadCubemap(vector<std::string> faces);
 
 float valueOfTextures = 0.3f;
 
-const unsigned int SCR_WIDTH = 1910.0f;
-const unsigned int SCR_HEIGHT = 1050.0f;
+const unsigned int SRC_WIDTH = 1910.0f;
+const unsigned int SRC_HEIGHT = 1050.0f;
 
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-float lastX = SCR_WIDTH / 2;
-float lastY = SCR_HEIGHT / 2;
+float lastX = SRC_WIDTH / 2;
+float lastY = SRC_HEIGHT / 2;
 
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
+
+int samples = 4;
 
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
@@ -37,7 +39,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Silent Hills", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SRC_WIDTH, SRC_HEIGHT, "Silent Hills", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -62,11 +64,36 @@ int main()
 						"C:/Users/antox/source/repos/OpenGLApplication/OpenGLApplication/planetshader.frag");
 	Shader rockShader("C:/Users/antox/source/repos/OpenGLApplication/OpenGLApplication/rockshader.vert",
 					  "C:/Users/antox/source/repos/OpenGLApplication/OpenGLApplication/rockshader.frag");
+	Shader screenShader("C:/Users/antox/source/repos/OpenGLApplication/OpenGLApplication/postprocessing_aa.vert",
+					    "C:/Users/antox/source/repos/OpenGLApplication/OpenGLApplication/postprocessing_aa.frag");
 
 	Model planetModel("C:/Libs/OpenGL/Models/planet/planet.obj");
 	Model rockModel("C:/Libs/OpenGL/Models/rock/rock.obj");
 
-	unsigned int amount = 10000;
+	float quadVertices[] = {
+		// positions   // texCoords
+		-1.0f,  1.0f,  0.0f, 1.0f,
+		-1.0f, -1.0f,  0.0f, 0.0f,
+		 1.0f, -1.0f,  1.0f, 0.0f,
+
+		-1.0f,  1.0f,  0.0f, 1.0f,
+		 1.0f, -1.0f,  1.0f, 0.0f,
+		 1.0f,  1.0f,  1.0f, 1.0f
+	};
+
+	unsigned int quadVAO, quadVBO;
+	glGenVertexArrays(1, &quadVAO);
+	glGenBuffers(1, &quadVBO);
+	glBindVertexArray(quadVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+	glBindVertexArray(0);
+
+	unsigned int amount = 1000;
 	glm::mat4* modelMatrices;
 	modelMatrices = new glm::mat4[amount];
 	srand(glfwGetTime());
@@ -80,7 +107,7 @@ int main()
 		float angle = (float)i / (float)amount * 360.0f;
 		float displacement = (rand() % (int)(2 * offset * 2000) / 100.0f - offset);
 		float x = sin(angle) * radius + displacement;
-		displacement = (rand() % (int)(2 * offset * 700) / 100.0f - offset);
+		displacement = (rand() % (int)(2 * offset * 1000) / 100.0f - offset);
 		float y = displacement * 0.4;
 		displacement = (rand() % (int)(2 * offset * 2000) / 100.0f - offset);
 		float z = cos(angle) * radius + displacement;
@@ -125,6 +152,51 @@ int main()
 		glBindVertexArray(0);
 	}
 
+	// MSAA
+	unsigned int fbo;
+	glGenFramebuffers(1, &fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+	
+		unsigned int tex;
+		glGenTextures(1, &tex);
+		glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, tex);
+		glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, samples, GL_RGB, SRC_WIDTH, SRC_HEIGHT, GL_TRUE);
+		glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, tex, 0);
+
+		unsigned int rbo;
+		glGenBuffers(1, &rbo);
+		glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+		glRenderbufferStorageMultisample(GL_RENDERBUFFER, samples, GL_DEPTH24_STENCIL8, SRC_WIDTH, SRC_HEIGHT);
+		glBindRenderbuffer(GL_RENDERBUFFER, 0);
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+			std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	// FBO for post-processing
+	unsigned int intermediateFBO;
+	glGenFramebuffers(1, &intermediateFBO);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, intermediateFBO);
+
+		unsigned int colorTex;
+		glGenTextures(1, &colorTex);
+		glBindTexture(GL_TEXTURE_2D, colorTex);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SRC_WIDTH, SRC_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorTex, 0);
+	
+		if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+			std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	screenShader.setInt("colorTex", 0);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		float currentFrame = glfwGetTime();
@@ -136,30 +208,52 @@ int main()
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.01f, 800.0f);
-		glm::mat4 view = camera.GetViewMatrix();
-		rockShader.use();
-		rockShader.setMat4("projection", projection);
-		rockShader.setMat4("view", view);
-		planetShader.use();
-		planetShader.setMat4("projection", projection);
-		planetShader.setMat4("view", view);
+		glBindFramebuffer(GL_FRAMEBUFFER, fbo);
 
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(50.0f, 0.0f, 50.0f));
-		model = glm::scale(model, glm::vec3(30.0f, 30.0f, 30.0f));
-		planetShader.setMat4("model", model);
-		planetModel.Draw(planetShader);
+			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			glEnable(GL_DEPTH_TEST);
 
-		rockShader.use();
-		rockShader.setInt("texture_diffuse1", 0);
-		glActiveTexture(GL_TEXTURE_2D);
-		glBindTexture(GL_TEXTURE_2D, rockModel.textures_loaded[0].id);
-		for (unsigned int i = 0; i < rockModel.meshes.size(); i++)
-		{
-			glBindVertexArray(rockModel.meshes[i].VAO);
-			glDrawElementsInstanced(GL_TRIANGLES, rockModel.meshes[i].indices.size(), GL_UNSIGNED_INT, 0, amount);
-		}
+			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SRC_WIDTH / (float)SRC_HEIGHT, 0.01f, 800.0f);
+			glm::mat4 view = camera.GetViewMatrix();
+			rockShader.use();
+			rockShader.setMat4("projection", projection);
+			rockShader.setMat4("view", view);
+			planetShader.use();
+			planetShader.setMat4("projection", projection);
+			planetShader.setMat4("view", view);
+
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, glm::vec3(50.0f, 0.0f, 50.0f));
+			model = glm::scale(model, glm::vec3(30.0f, 30.0f, 30.0f));
+			planetShader.setMat4("model", model);
+			planetModel.Draw(planetShader);
+
+			rockShader.use();
+			rockShader.setInt("texture_diffuse1", 0);
+			glActiveTexture(GL_TEXTURE_2D);
+			glBindTexture(GL_TEXTURE_2D, rockModel.textures_loaded[0].id);
+			for (unsigned int i = 0; i < rockModel.meshes.size(); i++)
+			{
+				glBindVertexArray(rockModel.meshes[i].VAO);
+				glDrawElementsInstanced(GL_TRIANGLES, rockModel.meshes[i].indices.size(), GL_UNSIGNED_INT, 0, amount);
+			}
+
+			glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo);
+			glBindFramebuffer(GL_DRAW_FRAMEBUFFER, intermediateFBO);
+			glBlitFramebuffer(0, 0, SRC_WIDTH, SRC_HEIGHT, 0, 0, SRC_WIDTH, SRC_HEIGHT, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glEnable(GL_DEPTH_TEST);
+
+		screenShader.use();
+		glBindVertexArray(quadVAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, colorTex);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
